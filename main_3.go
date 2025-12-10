018a079a6436a144a63bce4711bfc01d40d30447
CoffeeBaby <coffeebaby-be@gate.me>
2025-12-10 21:14:34 +0800

    feat: go 协程 chan 2

---
+++ main_3.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+var count int
+
+var done = make(chan bool, 100)
+
+func sayDoneNew(index int) {
+	done <- true
+	fmt.Printf("go %d done\n", index)
+}
+func waitUntilAllDoneNew(done chan bool, count int) {
+	for count > 0 {
+		if <-done {
+			count--
+		}
+	}
+}
+func main() {
+	fmt.Println("start....")
+	for i := 0; i < 5; i++ {
+		count++
+
+		go func(index int) {
+			defer sayDoneNew(index)
+			fmt.Printf("go %d running\n", index)
+		}(i)
+	}
+
+	waitUntilAllDoneNew(done, count)
+	fmt.Println("end....")
+}
